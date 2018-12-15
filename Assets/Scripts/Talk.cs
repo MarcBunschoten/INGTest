24eb723ebf0eba8b85a14ab31d154e5ca11d5b5c
DESKTOP\marcb <marc.bunschoten@gmail.com>
2018-12-15 17:34:47 +0100

    Hello on name

--- Assets/Scripts/Talk.cs
+++ Assets/Scripts/Talk.cs
@@ -7,14 +7,35 @@ namespace Zoo
 
     public class Talk : MonoBehaviour
     {
+        public string name = "";
 
         public void Speak()
         {
             var x = GameObject.FindObjectsOfType<Animal>();
-            foreach (var animal in x)
+            Debug.Log(name);
+            if (name == "")
             {
-                animal.GetComponent<Animal>().SayHello();
+                foreach (var animal in x)
+                {
+                    animal.GetComponent<Animal>().SayHello();
+                }
             }
+            else
+            {
+                foreach (var animal in x)
+                {
+                    if (animal.GetComponent<Animal>().name == name)
+                    {
+                        animal.GetComponent<Animal>().SayHello();
+                    }
+                }
+            }
+            
+        }
+
+        public void ChangeName(string value)
+        {
+            name = value;
         }
     }
 }
--- Readme.txt
+++ Readme.txt
@@ -9,4 +9,4 @@ Wat moet er allemaal kunnen:
 - VVVVV als je op Give meat drukt krijgen alle carnivores meat (en hun teksballon aanzetten).
 - VVVVV als je op Tricks drukt doen alle dieren die dat kunnen hun trucje.
 - VVVVV als je op Hello klikt zonder dat er een naam is ingevuld zeggen alle dieren hallo.
-- als er wel een naam is ingevuld zegtalleen dat dier hallo (de andere knoppen doen het nog wel voor alle dieren).
+- VVVVV als er wel een naam is ingevuld zegtalleen dat dier hallo (de andere knoppen doen het nog wel voor alle dieren).
